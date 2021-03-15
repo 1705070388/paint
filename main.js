0421be2f5a70ce844766feb842e91067b6df0531
1705070388 <75253725+1705070388@users.noreply.github.com>
2021-03-15 17:49:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var mouseEvent = "empty";
 var last_position_of_x, last_position_of_y;
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
-color = black;
+color = "black";
 width_of_line = 1;
 radius = 20;
 canvas.addEventListener("mousedown", my_mousedown);
